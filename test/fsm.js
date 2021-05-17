487a3e9984d75fe5fa6e1c02170202927e19d176
zyj3924 <1115025121@qq.com>
2021-05-17 20:08:08 +0800

    rename

--- test/fsm.js
+++ test/fsm.js
@@ -1,5 +1,11 @@
-let a = 0
-for(let i = 0; i < 5; i++) {
-    a = i++
-    console.log(a)
+const m = new Map()
+const f = function(){}
+m.set(1212, '121233')
+m.set(f,'is a function')
+console.log(m.get(f))
+for(let key of m){
+    console.log(key)
 }
+
+const  o = {}
+o['name'] = 'zyj'
